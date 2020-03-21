fd46c9c85e01fa150f97643105694c66334e4629
nik8singh <nscoder8@gmail.com>
2020-03-21 19:15:37 -0400

    original design

--- common/js/index.js
+++ common/js/index.js
@@ -63,33 +63,3 @@ $(document).ready(function() {
 
 
 });
-
-// $(function() {
-//   // Set the date we're counting down to
-//   const countDownDate = new Date("Dec 5, 2020 13:00:0").getTime();
-//
-// // Update the count down every 1 second
-//   var x = setInterval(function myTimer() {
-//
-//     // Get today's date and time
-//     var now = new Date().getTime();
-//
-//     // Find the distance between now and the count down date
-//     var distance = countDownDate - now;
-//
-//     // Time calculations for days, hours, minutes and seconds
-//     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//
-//     // Output the result in an element with id="demo"
-//     document.getElementById("days").innerHTML = days+"";
-//
-//     // If the count down is over, write some text
-//     if (distance < 0) {
-//       clearInterval(x);
-//       document.getElementById("days").innerHTML = "TODAY";
-//     }
-//     return myTimer;
-//   }(), 1000);
-
-// });
-
