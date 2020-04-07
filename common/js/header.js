16ff29cdac789425b48284557d49490b7faef755
nik8singh <nscoder8@gmail.com>
2020-04-07 00:03:10 -0400

    Completed : success message changes
    Next :
    Events
    Accommodation
    Footer
    comments from external user

---
+++ common/js/header.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+  const labels = document.querySelectorAll(".rsvpForm label");
+    let activelink;
+  labels.forEach(label => {
+    label.innerHTML = label.innerText
+      .split("")
+      .map(
+        (letter, idx) => `<span style="
+				transition-delay: ${idx * 50}ms
+			">${letter}</span>`
+      )
+      .join("");
+  });
+
+  let phoneNumber;
+  $("input:radio[name=response]").change(function() {
+    if (this.value === "1") {
+      $("#rsvp_phone").val(phoneNumber);
+      $("#phonePanel").slideDown();
+      $("#adultGuestPanel").slideDown();
+      $("#childGuestPanel").slideDown();
+      $("#visaPanel").slideDown();
+    } else if (this.value === "0") {
+      phoneNumber = $("#rsvp_phone").val();
+      $("#phonePanel").slideUp("slow", function() {
+        $("#rsvp_phone").val("000");
+      });
+      $("#adultGuestPanel").slideUp();
+      $("#childGuestPanel").slideUp();
+      $("#visaPanel").slideUp();
+    }
+  });
+
+  $("#myForm").on("submit", function(event) {
+    event.preventDefault();
+    let dataString = $(this).serialize();
+    let response = $("#yesorno").is(":checked");
+    dataString = dataString.concat("&yesorno=", response);
+    $.ajax({
+      type: "POST",
+      url: "common/PHP/index.php",
+      data: dataString,
+      success: function (data) {
+
+        $("#myForm")[0].reset();
+
+        if (response)
+          $("#successMessage").slideDown();
+        else
+          $("#sorryMessage").slideDown();
+
+        $("#rsvp_phone").val(phoneNumber);
+        $("#phonePanel").slideDown();
+        $("#adultGuestPanel").slideDown();
+          $("#childGuestPanel").slideDown();
+          $("#visaPanel").slideDown();
+      },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            $("#failMessage").slideDown();
+        }
+
+    });
+  });
+
+    const url = window.location.href;
+    const target = url.split('/');
+    $(".nav-link").each(function () {
+        if ($(this).attr('href') === (target[target.length - 1])) {
+            $("#navbarSupportedContent>ul>li.active").removeClass("active");
+            $(this).parent().addClass('active');
+        }
+    });
+
+});
---
+++ common/js/mainCommon.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("#mainHeader").load("header.html");
+
+});
+
+
